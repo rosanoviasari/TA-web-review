e17063e8a0678c600b6cf7f83b83415ee41533df
rosa <rosanoviasari@gmail.com>
2024-10-22 11:45:23 +0700

    update

--- views/dashboard.php
+++ views/dashboard.php
@@ -7,8 +7,6 @@ JOIN tb_kategori AS k ON p.kategori_product = k.id
 JOIN tb_brand AS b ON p.brand_product = b.id 
 JOIN tb_notes_parfume AS n ON p.notes_parfume = n.id");
 
-var_dump($data);
-
 
 ?>
 
@@ -32,7 +30,7 @@ var_dump($data);
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav ms-auto">
             <li class="nav-item">
-                <a class="nav-link" href="login.php">Login</a>
+                <a class="nav-link" href="./user/login.php">Login</a>
             </li>
             </ul>
         </div>
@@ -40,49 +38,37 @@ var_dump($data);
     </nav>
     <!-- navbar end -->
     <div class="content-section">
-        <div class="container py-3">
-            <div class="card">
-                <div class="card-body">
-                    <h3>Article List</h3>
-                    <div class="row mb-4">
-                        <?php foreach($data as $row):?>
-                        <!-- kolom products -->
-                        <div class="col-sm-3 col-lg-3 mb-4">
-                            <div class="card" style="width: 18rem;">
-                                <img src="<?= $row['gambar_product'] ?>" class="card-img-top" alt="...">
-                                <div class="card-body">
-                                  <h5 class="card-title"><?= $row['nama_product'];?></h5>
-                                  <p class="card-text">Notes Perfume <?= $row['notes'] ?></p>
-                                  <a href="./detail.html" class="btn btn-primary">Read Review</a>
-                                </div>
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="" class="active" aria-current="" aria-label="Slide"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item">
+                <div class="row">
+                    <div class="col-4 mb-4">
+                        <div class="card">
+                            <img src="<?= $data[$col]['gambar_product'] ?>" class="card-img-top" alt="...">
+                            <div class="card-body">
+                            <h5 class="card-title"><?= $data[$col]['nama']; ?></h5>
+                            <p class="card-text">Notes Perfume <?= $data[$col]['notes'] ?></p>
+                            <a href="./detail.html" class="btn btn-primary">Read Review</a>
                             </div>
-                            <?php endforeach; ?>
-                        </div>                    
-                    <!-- end of products -->
-                    </div>
-                    <div class="row mb-4">
-                        <nav aria-label="Page navigation example">
-                            <ul class="pagination justify-content-center">
-                                <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-                                <li class="page-item"><a class="page-link" href="#">1</a></li>
-                                <li class="page-item"><a class="page-link" href="#">2</a></li>
-                                <li class="page-item"><a class="page-link" href="#">3</a></li>
-                                <li class="page-item"><a class="page-link" href="#">Next</a></li>
-                            </ul>
-                        </nav>                        
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-        <!-- End of content section -->
     </div>
 
     <!--Footer Start-->
-<footer class="my-footer">
-    <marquee>
-        <address>&copy;2024 - All Right Reserved</address>
-    </marquee>
-</footer>
+    <div class="card">
+        <div class="card-footer">
+        <marquee>
+            <address>&copy;2024 - All Right Reserved</address>
+        </marquee>
+        </div>
+    </div>
     <!-- End of Footer -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
--- views/user/login.php
+++ views/user/login.php
@@ -8,7 +8,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login / Register</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
     <!-- navbar start -->
@@ -34,136 +35,24 @@
                 <div class="card-body">
                     <div class="row">
                         <div class="col-sm-12 mt-4 mb-3" align="center">
-                            <!-- Avatar -->
-                            <img src="./assets/image/rosa.jpeg" class="avatar">
-                            <h1 class="text-bold mb-4">Rosa Noviasari</h1>
+                            <h1 class="text-bold mb-4">Login / Register</h1>
                             <hr class="mt-4 mb-2">
                         </div>
                         <div class="col-12 col-sm-6 ">
                             <!-- About -->
                             <div class="row">
-                                <div class="col-12 mb-3">
-                                    <h4 class="mb-3">Tentang Saya</h4>
-                                    <p>
-                                        Saya adalah seorang guru coding di sebuah lembaga bimbingan belajar.
-                                        Kegiatan saya sehari-hari adalah mengajar, dan membuat project yang akan dipelajari oleh siswa.
-                                        Sebagai seorang guru, saya senang belajar tentang hal baru.
-                                    </p>
-                                </div>
-                                <div class="col-5">
-                                    Nama
-                                </div>
-                                <div class="col-7">
-                                    Rosa Noviasari
-                                </div>
-                                <div class="col-5">
-                                    Tempat Tanggal Lahir
-                                </div>
-                                <div class="col-7">
-                                    Bandung 27 November 1999
-                                </div>
-                                <div class="col-5">
-                                    Alamat
-                                </div>
-                                <div class="col-7">
-                                    Jl Kosambi Bandung
-                                </div>
-                                <div class="col-5">
-                                    No Handphone
-                                </div>
-                                <div class="col-7">
-                                    <a href="tel:+6285795222039">085795222039</a>
-                                </div>
-                                <div class="col-5">
-                                    Surel
-                                </div>
-                                <div class="col-7 mb-4">
-                                    <a href="mailto:rosanoviasari@gmail.com">rosanoviasari@gmail.com</a>
-                                </div>
+                            <div class="input-group mb-3">
+                                <span class="input-group-text" id="inputGroup-sizing-default">Username</span>
+                                <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
                             </div>
-                            <!-- End of About -->
-
-                            <!-- Socmed info -->
-                            <div class="socmed-container">
-                                <ul class="social-list">
-                                    <li class="social-item">
-                                        <a href="https://www.facebook.com/rosa.noviasari/"><i class="fa-brands fa-facebook"></i>Facebook</a>
-                                    </li>
-                                    <li class="social-item">
-                                        <a href="https://www.instagram.com/rosanoviasari/"><i class="fa-brands fa-instagram"></i>Instagram</a>
-                                    </li>
-                                    <li class="social-item">
-                                        <a href="https://id.linkedin.com/in/rosa-noviasari"><i class="fa-brands fa-linkedin"></i>LinkedIn</a>
-                                    </li>
-                                </ul>
                             </div>
-                            <!-- End of Social Media -->
+                            <div class="row">
+                            <div class="input-group mb-3">
+                                <span class="input-group-text" id="inputGroup-sizing-default">Password</span>
+                                <input type="password" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                            </div>
+                            </div>
                         </div>
-                        <!-- My Experience -->
-                        <div class="col-12 col-sm-6">
-                            <h4>My Experience</h4>
-                            <ul class="job-list">
-                                <li class="job-title">
-                                    Educourse.id - Coding Teacher for K-12 (2023-Sekarang)
-                                    <ul class="job-desc">
-                                        <li>
-                                            Membuat dokumen administratif pembelajaran (perencanaan, project, rapor)
-                                        </li>
-                                        <li>
-                                            Melakukan tutoring pembelajaran coding untuk siswa usia K-12
-                                        </li>
-                                        <li>
-                                            Melakukan promosi program melalui event
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li class="job-title">
-                                    TK Negeri Centeh - Staff Administrasi  (2021-2022)
-                                    <ul class="job-desc">
-                                        <li>
-                                            Membuat kelengkapan administrasi seperti surat, laporan kegiatan, dan laporan keuangan
-                                        </li>
-                                        <li>
-                                            Melakukan pencatatan surat masuk dan surat keluar
-                                        </li>
-                                        <li>
-                                            Membuat dokumentasi sekolah seperti konten instagram, video youtube, dan tiktok
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li class="job-title">
-                                    Directorate of International Affairs - Email and Administration Staff  (2021)
-                                    <ul class="job-desc">
-                                        <li>
-                                            Membuat pencatatan surat masuk dan surat keluar
-                                        </li>
-                                        <li>
-                                            Membuat dokumen MoU antara pihak kampus dengan kampus luar negeri
-                                        </li>
-                                        <li>
-                                            Melaksanakan acara pertukaran budaya antar kampus UPI, Hirosima university, dan Universiti Sains Malaysia
-                                        </li>
-                                    </ul>
-                                </li>
-                            </ul>
-                            <h4>Tools Used</h4>
-                             <!-- Tools Used -->
-                             <div class="program-container">
-                                <ul class="program-list">
-                                    <li class="program-item">
-                                        <a href=""><i class="fa-brands fa-html5"></i></a>
-                                    </li>
-                                    <li class="program-item">
-                                        <a href=""><i class="fa-brands fa-css3-alt"></i></a>
-                                    </li>
-                                    <li class="program-item">
-                                        <a href=""><i class="fa-brands fa-github"></i></a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <!-- End of Tools Used -->
-                        </div>
-                        <!-- End of My Experience -->
                     </div>
                 </div>
             </div>
@@ -172,11 +61,13 @@
     </div>
 
     <!--Ini Footer-->
-    <footer class="my-footer">
+    <div class="card">
+        <div class="card-footer">
         <marquee>
             <address>&copy;2024 - All Right Reserved</address>
         </marquee>
-    </footer>
+        </div>
+    </div>
     <!-- End of Footer -->        
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="./assets/bootstrap/js/bootstrap.min.js"></script>
