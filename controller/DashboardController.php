05a8a0a66aef1745c5c88a1578987ccae1654ba5
Rosa <rosa@MacBook-Pro-Rosa.local>
2024-10-23 16:04:28 +0700

    update

--- controller/DashboardController.php
+++ controller/DashboardController.php
@@ -1,99 +1,90 @@
 <?php
 
-require '';
-
 function getProduct(){	
     $dataProduct = [
         [
-            "id" => "1"
-            "gambar_product" => "asset/gambar/Alchemist_Galleria.png"
+            "id" => "1",
+            "gambar_product" => "asset/gambar/Alchemist_Galleria.png",
             "nama_product"=> "Galleria",
             "kategori"=> "Eau de Parfume",
             "nama_brand"=> "Alchemist",
-            "nama_notes_parfume" => "Gourmand"
+            "nama_notes_parfume" => "Gourmand",
         ],
         [
-            "id" => "2"
-            "gambar_product" => "asset/gambar/ALT_Moroccansunset.jpg"
+            "id" => "2",
+            "gambar_product" => "asset/gambar/ALT_Moroccansunset.jpg",
             "nama_product"=> "Moroccan Sunset",
             "kategori"=> "Extrait de Parfume",
             "nama_brand"=> "ALT",
-            "nama_notes_parfume" => "Spices"
+            "nama_notes_parfume" => "Spices",
         ],
         [
-            "id" => "3"
-            "gambar_product" => "asset/gambar/BOHE_Scentofheaven.jpg"
+            "id" => "3",
+            "gambar_product" => "asset/gambar/BOHE_Scentofheaven.jpg",
             "nama_product"=> "Scent of Heaven",
             "kategori"=> "Extrait de Parfume",
             "nama_brand"=> "BOHE Bali",
-            "nama_notes_parfume" => "Floral"
+            "nama_notes_parfume" => "Floral",
         ],
         [
-            "id" => "4"
-            "gambar_product" => "asset/gambar/Boura_Noisy.jpg"
+            "id" => "4",
+            "gambar_product" => "asset/gambar/Boura_Noisy.jpg",
             "nama_product"=> "Noisy Bathroom",
             "kategori"=> "Extrait de Parfume",
             "nama_brand"=> "Boura",
-            "nama_notes_parfume" => "Musk"
+            "nama_notes_parfume" => "Musk",
         ],
         [
-            "id" => "5"
-            "gambar_product" => "asset/gambar/HINT_Identity.jpg"
+            "id" => "5",
+            "gambar_product" => "asset/gambar/HINT_Identity.jpg",
             "nama_product"=> "Identity",
             "kategori"=> "Eau de Parfume",
             "nama_brand"=> "HINT",
-            "nama_notes_parfume" => "Woods"
+            "nama_notes_parfume" => "Woods",
         ],
         [
-            "id" => "6"
-            "gambar_product" => "asset/gambar/HMNS_Alpha.jpg"
+            "id" => "6",
+            "gambar_product" => "asset/gambar/HMNS_Alpha.jpg",
             "nama_product"=> "Alpha",
             "kategori"=> "Extrait de Parfume",
             "nama_brand"=> "HMNS",
-            "nama_notes_parfume" => "Fruity"
+            "nama_notes_parfume" => "Fruity",
         ],
         [
-            "id" => "7"
-            "gambar_product" => "asset/gambar/Mine_Luciddreams.png"
+            "id" => "7",
+            "gambar_product" => "asset/gambar/Mine_Luciddreams.png",
             "nama_product"=> "Lucid Dreams",
             "kategori"=> "Eau de Parfume",
             "nama_brand"=> "MINE",
-            "nama_notes_parfume" => "Gourmand"
+            "nama_notes_parfume" => "Gourmand",
         ],
         [
-            "id" => "8"
-            "gambar_product" => "asset/gambar/MyKonos_Aphrodite.jpg"
+            "id" => "8",
+            "gambar_product" => "asset/gambar/MyKonos_Aphrodite.jpg",
             "nama_product"=> "Aphrodite",
             "kategori"=> "Extrait de Parfume",
             "nama_brand"=> "MyKonos",
-            "nama_notes_parfume" => "Musk"
+            "nama_notes_parfume" => "Musk",
         ],
         [
-            "id" => "9"
-            "gambar_product" => "asset/gambar/ReadySet_windrises.jpg"
+            "id" => "9",
+            "gambar_product" => "asset/gambar/ReadySet_windrises.jpg",
             "nama_product"=> "Windrises",
             "kategori"=> "Eau de Toilette",
             "nama_brand"=> "Readyset",
-            "nama_notes_parfume" => "Floral"
+            "nama_notes_parfume" => "Floral",
         ],
         [
-            "id" => "10"
-            "gambar_product" => "asset/gambar/SaffnCo_SOTB.jpg"
+            "id" => "10",
+            "gambar_product" => "asset/gambar/SaffnCo_SOTB.jpg",
             "nama_product"=> "S.O.T.B",
             "kategori"=> "Extrait de Parfume",
             "nama_brand"=> "Saff&Co",
-            "nama_notes_parfume" => "Musk"
+            "nama_notes_parfume" => "Musk",
         ],
     ]
     return $dataProduct;
 }
 
-getProduct = myquery("SELECT p.id, p.gambar_product, p.nama_product, k.kategori, b.nama_brand, n.nama_notes_parfume 
-FROM tb_product AS p 
-JOIN tb_kategori AS k ON p.kategori_product = k.id 
-JOIN tb_brand AS b ON p.brand_product = b.id 
-JOIN tb_notes_parfume AS n ON p.notes_parfume = n.id");{
-
-}
 
 ?>
